ae309e4c32c16045d1cf129cde9b33d6e0c303bd
Gabe Payne <gabepayne@Gabes-MacBook-Pro.local>
2025-06-09 11:09:33 +0100

    basic use of camera added

--- frontend/app/(tabs)/community.tsx
+++ frontend/app/(tabs)/community.tsx
@@ -3,10 +3,21 @@ import { StyleSheet, View } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { Button } from "@react-navigation/elements";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { ScrollView } from "react-native";
 
+// Define RootStackParamList or import it from your navigation types file
+type RootStackParamList = {
+  UploadPost: undefined;
+  // Add other routes here as needed
+};
 
 export default function TabTwoScreen() {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
+    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.scrollcontainer}>
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
@@ -34,20 +45,43 @@ export default function TabTwoScreen() {
           community section with usr of the month or something, able to comment, like, see location?
         </ThemedText>
       </View>
-      <Button color="black">post photo</Button>
+     
       <ThemedText>leave comments and likes on peoples posts</ThemedText>
       <ThemedText>
         fun little feature, most liked photo /user of the week maybe
       </ThemedText>
+      <Button
+  color="black" onPress={() => navigation.navigate("UploadPost")}>Post Photo</Button>
       <ThemedText>search bar to explore by desitnation or mushroom</ThemedText>
       <ThemedText>
         archive of other peoples locations/mushrooms etcs
       </ThemedText>
     </ParallaxScrollView>
+    </ScrollView>
+    {/* <View style={styles.footer}>
+    
+
+    </View> */}
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    padding: 10,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+  },
+  scrollcontainer:{
+    paddingBottom: 80
+  },
+  container:{
+    flex: 1
+  },
   headerImage: {
     color: "#808080",
     bottom: -90,
@@ -60,6 +94,8 @@ const styles = StyleSheet.create({
   },
   buttonTitle: {
     color: "#808080",
+    position: "absolute",
+    bottom: 0,
   },
   reactLogo: {
     width: 630,
