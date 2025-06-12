ec0555666333a0bb80cee8ae423cc29123440a33
Gabe Payne <gabepayne@Gabes-MacBook-Pro.local>
2025-06-12 08:29:25 +0100

    styling changes to the home page

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -20,14 +20,14 @@ export default function HomeScreen() {
 </View>
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">CAP CHECK 🍄</ThemedText>
-      </ThemedView>
+      
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">
-      Is your pocket guide to mushroom foraging fun! Snap, log, and share your fungi finds while connecting with fellow fungi finders on the community page. Whether you're spotting something strange or showing off a rare cap, CapCheck makes mushroom adventures even more magical.
-        </ThemedText>
-      </ThemedView>
+  <View style={styles.textBox}>
+    <ThemedText type="subtitle" style={styles.blackText}>
+      This is your pocket guide to mushroom foraging fun! Snap, log, and share your fungi finds while connecting with fellow fungi finders on the community page. Whether you're spotting something strange or showing off a rare cap, CapCheck makes mushroom adventures even more magical.
+    </ThemedText>
+  </View>
+</ThemedView>
       
       <View style={styles.mushroomFact}>
         <Randomiser />
@@ -40,6 +40,10 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  blackText: {
+    color: 'black',
+    textAlign: 'justify',
+  },
   widget: {
     width: "100%"
   },
@@ -65,8 +69,19 @@ const styles = StyleSheet.create({
   marginTop: 40,
 },
 logoContainer: {
-  backgroundColor: "#f5f5f5", 
+  backgroundColor: "#fdf8ef", 
   alignItems: "center",
   paddingVertical: 10,
 },
+textBox: {
+    backgroundColor: '#f5f5f5',
+    padding: 16,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, 
+    width: '100%',
+  },
 });
